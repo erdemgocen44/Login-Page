0cafe31c924c6d239573d89ffab9fdfe7896c6bc
Erdem <erdemgocen44@gmail.com>
2023-03-04 19:19:34 +0100

    photo est ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:login_accueil/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Login Page',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      home: LoginPage(),
     );
   }
 }
